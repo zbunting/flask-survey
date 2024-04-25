1ec65d20a6cb21fb7ee1357b7f149827334b7e15
Zach Bunting <zvrbunting@gmail.com>
2024-04-25 12:32:01 -0700

    WIP.

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ responses = []
 @app.get('/')
 def index():
     """Renders the main survey page"""
-    # TODO: change name of survey and add instructions
-    return render_template("survey_start.jinja")
+
+    return render_template("survey_start.jinja", survey=survey)
 
 
 @app.post('/begin')
@@ -28,6 +28,30 @@ def redirect_to_questions():
 @app.get('/questions/<int:question_num>')
 def generate_questions(question_num):
     """Generates page for specific question """
+
+    # TODO: make sure continue can't be pressed unless
+    # an option was selected
     question = survey.questions[question_num]
 
-    return render_template('question.jinja', question=question)
+    return render_template(
+        'question.jinja',
+        question=question,
+        question_num=question_num)
+
+
+@app.post('/answer/<int:question_num>')
+def handle_answer(question_num):
+    """Store answer and redirect to next questions in survey"""
+
+    # TODO: make sure we can't go out of bounds in our
+    # question list
+
+    next_question_num = str(question_num + 1)
+    answer = request.form.get("answer")
+
+    # does the html stop you from pressing continue without
+    # selection an option
+
+    responses.append(answer)
+
+    return redirect(f"/questions/{next_question_num}")
