9a439f0953c11a0c4f0238bf18908e942523f185
Zach Bunting <zvrbunting@gmail.com>
2024-04-25 11:48:32 -0700

    Finished up to step 2.

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get('/')
+def index():
+    """Renders the main survey page"""
+
+    return render_template("survey_start.jinja")
+
+@app.post('/begin')
+def redirect_to_questions():
+    """Redirect to the questions page"""
+
+    return redirect("/questions")
+
