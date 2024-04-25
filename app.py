01f455dbdfc3f9db941688c2bd32c5c5569b65d4
Zach Bunting <zvrbunting@gmail.com>
2024-04-25 11:23:42 -0700

    Source code

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
